6de1abe5b19d0c0d7e09000c080fe17ead9ed83b
William Gallios <williamgallios@gmail.com>
2014-01-06 17:18:02 -0800

    update to homepage

--- application/views/welcome/index.php
+++ application/views/welcome/index.php
@@ -4,3 +4,7 @@
     <h1>Welcome</h1>
     <p class='lead'>Welcome to Price Tracker. </p>
 </div>
+
+<hr>
+
+<p class='lead'>ProductPriceTracker.com is the best resource online to tracke prices on amazon<sup>&reg;</sup>.</p>
